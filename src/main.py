0aafd48b2208b6a5a8e40b79b618beef4666c3e4
Adem Saglam <asaglam84@hotmail.com>
2020-12-13 22:29:44 +0000

    mode changes

--- src/main.py
+++ src/main.py
@@ -13,10 +13,8 @@ def main():
     Available Modes
     ---------------------------
 
-    1 : Keyboard teleop
-    2 : Follow way points
-    3 : Create way points
-    9 : Simple move with /cmd_vel
+    1 : Follow way points
+    2 : Create way points
 
     CTRL-C to quit
     """
@@ -35,16 +33,11 @@ def main():
             stop_threads = True
             if active_thread:
                 active_thread.join()
-        elif int(mode) == 3:
+        elif int(mode) == 2:
             active_thread = threading.Thread(target=app.mode_waypoint_create, args=(lambda : stop_threads, ))
             active_thread.start()
-        elif int(mode) == 2:
-            active_thread = threading.Thread(target=app.start_patrolling, args=(lambda : stop_threads, ))
-            active_thread.start()
         elif int(mode) == 1:
-            active_thread = threading.Thread(target=app.basic_move)
-            active_thread.start()
-        elif int(mode) == 0:
-            print("0")
+            active_thread = threading.Thread(target=app.start_patrolling, args=(lambda : stop_threads, ))
+            active_thread.start()                
 
 main()
